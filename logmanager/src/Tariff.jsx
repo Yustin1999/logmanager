2beeefd40c93b891cb46132a447598a0d752f905
Yustin1999 <justinsummers@outlook.com>
2025-08-07 14:02:23 +0100

    Display XML on page
    XML is now displaying on the page.

--- logmanager/src/Tariff.jsx
+++ logmanager/src/Tariff.jsx
@@ -1,11 +1,21 @@
+import { useEffect, useState } from 'react';
+
 export default function Tariff() {
-    //need to add the tariff data here, would be nice to include a date it was last changed possibly a change log
+    const [xmlContent, setXmlContent] = useState('');
+    useEffect(() => {
+        fetch('/Tariff.xml')
+            .then((res) => res.text())
+            .then((text) => setXmlContent(text))
+            .catch((error) => console.error('Error fetching XML:', error));
+    }, []);
     
     return (
         <div >
-            <h1>PANCAKES</h1>
-            <iframe src="C:\Users\JustinSummers\Documents\GitHub\logmanager\logmanager\public\Tariff.xml" width="100%" height="800px" title="Tariff XML"/>
+            <pre style={{ whiteSpace: 'pre-wrap', wordWrap: 'break-word', background: '#f4f4f4', padding: '1rem', fontSize:'10px', width:'50%'}}>
+                {xmlContent}
+            </pre>
         </div>
 
     )
 }
+//need to add the tariff data here, would be nice to include a date it was last changed possibly a change log. A tariff tester would be very useful but i dont know how complicated this is.
