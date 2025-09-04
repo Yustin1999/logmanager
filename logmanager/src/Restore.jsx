f4c310b3a11b662c5cf91aad375b618cea4f0d1a
Yustin1999 <justinsummers@outlook.com>
2025-09-04 06:44:26 +0100

    Supabase changes

--- logmanager/src/Restore.jsx
+++ logmanager/src/Restore.jsx
@@ -4,14 +4,15 @@ export default function Restore() {
     const [rows, setRows] = useState([])
     const [log, setLog] = useState([])
     const handleClick =  async (row) => {
-        const updatedRow = { ...row, authorization: row.authorization === 1 ? 0 : 1 };
+        const updatedRow = { ...row, is_authorised: row.is_authorised === 1 ? 0 : 1 };
         console.log(updatedRow)
-        setLog({ username: updatedRow.username, email: updatedRow.email, prevAuth: (updatedRow.authorization === 1 ? "True" : "False"), currentAuth: (updatedRow.authorization === 1 ? "False" : "True") })
+        setLog({ username: updatedRow.username, email: updatedRow.email, prevAuth: (updatedRow.is_authorised === 1 ? true : false), currentAuth: (updatedRow.is_authorised === 1 ? false : true) })
         
         
         
         try {
             const res = await fetch("https://backendproject-it4q.onrender.com/api/updateUser", {
+            //const res = await fetch("http://localhost:4000/api/updateUser", { 
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -19,7 +20,7 @@ export default function Restore() {
                 body: JSON.stringify(updatedRow) // send JSON to backend
             });
             const res2 = await fetch("https://backendproject-it4q.onrender.com/api/userLog", {
-             //const res2 = await fetch("http://localhost:4000/api/userLog"   
+            //const res2 = await fetch("http://localhost:4000/api/userLog", {  
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
@@ -33,7 +34,7 @@ export default function Restore() {
             console.log("Response from server:", data2);
             setRows(prevRows =>
                 prevRows.map(r =>
-                    r.id === row.id ? { ...r, authorization: r.authorization === 1 ? 0 : 1 } : r,
+                    r.id === row.id ? { ...r, is_authorised: r.is_authorised === 1 ? 0 : 1 } : r,
                     //setLog([row.username, row.email, (row.authorization === 1 ? "True" : "False"), (row.authorization === 1 ? "False" : "True")])
                 )
                
@@ -80,8 +81,8 @@ export default function Restore() {
                                 <td>{row.id}</td>
                                 <td>{row.username}</td>
                                 <td>{row.email}</td> 
-                                <td>{row.authorization == 0 ? "Yes" : "No"}</td>
-                                <td>{row.authorization == 0 ? <button onClick={() => handleClick(row)}>Deauthorise</button> : <button  onClick={() => handleClick(row)}>Authorise</button>}</td>
+                                <td>{row.is_authorised == 0 ? "Yes" : "No"}</td>
+                                <td>{row.is_authorised == 0 ? <button onClick={() => handleClick(row)}>Deauthorise</button> : <button  onClick={() => handleClick(row)}>Authorise</button>}</td>
                             </tr>
                         ))
                     ) : (
