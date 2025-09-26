1c65d6f9638e81a466c96760f6d1f969183671cc
Yustin1999 <justinsummers@outlook.com>
2025-09-26 08:20:48 +0100

    remove log spam

--- logmanager/src/TokenCheck.jsx
+++ logmanager/src/TokenCheck.jsx
@@ -10,10 +10,10 @@ export default function PrivateRoute({ children }) {
     
     const now = Math.floor(Date.now() / 1000);
     const { user } = useAuth();
-    console.log("PrivateRoute user:", user);
-    // If user exists in context, render children
+    //console.log("PrivateRoute user:", user);
+    
     if (user && user.exp && user.exp > now) return children;
 
-    // Otherwise redirect to login
+    
     return <Navigate to="/login" replace />;
 }
