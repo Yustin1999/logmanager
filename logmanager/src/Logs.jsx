65887a4513bf37943f57d15cf619e139f6bd0fff
Yustin1999 <justinsummers@outlook.com>
2025-09-26 06:52:38 +0100

    Save all

--- logmanager/src/Logs.jsx
+++ logmanager/src/Logs.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 /* 
     - This page just generates the URL's and DIVs for each of the pages on the site
 */
+
 export default function Logs() {
     //Can adjust this to pull the logs from a backend or a file
     const folders = ["Scoreboard","WSExit","WSEntry","WSEntryExit","WSMachineData","Misc","Archive","Restore","History"]
