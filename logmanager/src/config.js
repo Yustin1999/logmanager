b9d1e7ff17a920ccfe0e6fd575e3fe526ce34ec3
Yustin1999 <justinsummers@outlook.com>
2025-10-10 08:33:39 +0100

    Update config.js

--- logmanager/src/config.js
+++ logmanager/src/config.js
@@ -1,5 +1,5 @@
 // Config page to contain any URLs and information that is called throughout the project
 
 
-//export const API_URL = "https://backendproject-it4q.onrender.com/api/"
-export const API_URL = "http://localhost:4000/api/"
+export const API_URL = "https://backendproject-it4q.onrender.com/api/"
+//export const API_URL = "http://localhost:4000/api/"
