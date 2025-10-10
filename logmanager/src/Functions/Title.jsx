b79beb8ef2ada59be76f89a768ae78bfe042d82f
Yustin1999 <justinsummers@outlook.com>
2025-10-10 10:32:43 +0100

    styling

--- logmanager/src/Functions/Title.jsx
+++ logmanager/src/Functions/Title.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Title() {
 
     return (
-        <div className = "title" href = "https://logmanager.vercel.app/" >
+        <div className = "title-div" href = "https://logmanager.vercel.app/" >
             <h1 className="title">Log Manager</h1>
             <Link className="title-button"  to={"/Tech"}>
                 <button>Tech Area</button>
