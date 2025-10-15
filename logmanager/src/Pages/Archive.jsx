5820a9ba9c9a99b8dcde0227d7896e101d4097ce
Yustin1999 <justinsummers@outlook.com>
2025-10-15 15:33:18 +0100

    Update Archive.jsx

--- logmanager/src/Pages/Archive.jsx
+++ logmanager/src/Pages/Archive.jsx
@@ -29,7 +29,7 @@ export default function Archive() {
                 ]);
 
 
-                console.log("Dropdown array:", dropdownArray);
+                
 
                 setDropDownBool(dropdownArray);
                 
@@ -50,7 +50,7 @@ export default function Archive() {
             {files.map(file => (
                 <ul onMouseLeave={() => { setDropDownHovered(null); setDropDownClicked(null); }}  className="logs-list">
                     
-                    <h2>{dropDownBool[0].formattedDate}</h2>
+                    <h2>{dropDownBool[0][0]}</h2>
                         <h1 onMouseEnter={() => setDropDownHovered(file[file.length - 1].formattedDate)}>{file[file.length - 1].formattedDate}{dropDownHovered === file[file.length - 1].formattedDate ? <img className="dropdown" onClick={() => setDropDownClicked(!dropDownClicked) } src={Downarrow} /> :""}</h1>
                         {dropDownHovered === file[file.length - 1].formattedDate && dropDownClicked && (
 
