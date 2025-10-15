5cf70a5ad38da098cb9b328c08d961569ad21de4
Yustin1999 <justinsummers@outlook.com>
2025-10-15 14:03:01 +0100

    Dropdown Changes

--- logmanager/src/Pages/Archive.jsx
+++ logmanager/src/Pages/Archive.jsx
@@ -5,14 +5,16 @@ import Downarrow from "../assets/DropDown.svg"
 export default function Archive() {
     const [files, setFiles] = useState([]);
     const [isHovered, setIsHovered] = useState(null);
-    const [dropDownHovered, setDropDownHovered] = useState(null)
-    const [dropDownClicked, setDropDownClicked] = useState(null)
+    const [dropDownHovered, setDropDownHovered] = useState(null);
+    const [dropDownClicked, setDropDownClicked] = useState(null);
+    const [dropdownBool, setDropdownBool] = useState([]);
     const handleClick = (e, file) => {
         if (!file) return;
         //console.log(file);
         window.location.href = API_URL + `folder/${encodeURIComponent(file)}`;
     }
-   
+    
+
     
 
     useEffect(() => {
@@ -27,7 +29,7 @@ export default function Archive() {
     }, []);
 
 
-    
+    //Possible solutions add a true/false to each date object to check if its clicked or not when true it will render the dropdown
 
 
 
@@ -36,9 +38,9 @@ export default function Archive() {
         <div className="archive-page">
            
             {files.map(file => (
-                <ul key={file[0]} onMouseLeave={() => { setDropDownHovered(null); setDropDownClicked(null) }}  className="logs-list">
-                    
-                   
+                <ul onMouseLeave={() => { setDropDownHovered(null); setDropDownClicked(null); }}  className="logs-list">
+                    {setDropdownBool([...dropdownBool, [file[file.length - 1].formattedDate, file[file.length - 1].state]])}
+                    {console.log(dropdownBool)}
                         <h1 onMouseEnter={() => setDropDownHovered(file[file.length - 1].formattedDate)}>{file[file.length - 1].formattedDate}{dropDownHovered === file[file.length - 1].formattedDate ? <img className="dropdown" onClick={() => setDropDownClicked(!dropDownClicked) } src={Downarrow} /> :""}</h1>
                         {dropDownHovered === file[file.length - 1].formattedDate && dropDownClicked && (
 
