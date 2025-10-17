8303c7fb85189b46696a03cb6e4d5886343ae8fb
Yustin1999 <justinsummers@outlook.com>
2025-10-17 15:05:18 +0100

    Hide button on login page

--- logmanager/src/Functions/Title.jsx
+++ logmanager/src/Functions/Title.jsx
@@ -1,13 +1,17 @@
 import { Link } from "react-router-dom";
-
+import { useLocation } from "react-router-dom";
 export default function Title() {
+    
+    const location = useLocation();
 
     return (
         <div className = "title-div" href = "https://logmanager.vercel.app/" >
             <h1 className="title">Log Manager</h1>
+            {location.pathname === "/login" ? null : 
             <Link className="title-button"  to={"/Tech"}>
                 <button className="tech-button">Tech Area</button>
             </Link>
+            }
         </div >
     );
 };
